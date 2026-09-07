7e4f999fe38c86ddb98f35711cd8fc3d1b89a1f6
ornella.petrelli <ornella.petrelli@gmail.com>
2026-09-07 14:08:36 -0300

    acomodo carpetas

---
+++ app/database.py
@@ -0,0 +1,27 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("No se encontró DATABASE_URL en el archivo .env")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+try:
+    with engine.connect() as connection:
+        print("PostgreSQL conectado correctamente")
+except Exception as e:
+    print("Error:", e)
