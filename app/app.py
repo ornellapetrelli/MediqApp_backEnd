1861d0568bf2cc5efc7c150633507967af00028d
ornella.petrelli <ornella.petrelli@gmail.com>
2026-09-10 17:44:51 -0300

    agregar rutas de profesionales

--- app/app.py
+++ app/app.py
@@ -1,13 +1,14 @@
 from app.database import Base, engine
 from fastapi import FastAPI
 from app.routers.auth_router import router as auth_router
-
+from app.routers.professional import router as professional_router
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 
 app.include_router(auth_router)
+app.include_router(professional_router)
 
 @app.get("/")
 def root():
